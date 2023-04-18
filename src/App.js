35646ab6f35e2819a5caa8b6bfd2cd70c4f15c00
Daniil <daniil.kravchenko.92@yandex.ru>
2023-04-18 22:33:09 +0200

    react first steps

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useRef } from "react";
+import TodoList from "./TodoList";
+import uuidv4 from 'uuid/v4'
+function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if(name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete:false}]
+    })
+    todoNameRef.current.value = null
+  }
+  return (
+    <>
+    <TodoList todos={todos}/>
+    <input ref={todoNameRef}type="text" />
+    <button onClick={handleAddTodo}>Add todo</button>
+    <button>Clear completed todos</button>
+    <div> 0 left to do</div>
+    </>
+  )
+}
+
+export default App;
